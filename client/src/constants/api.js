da475aa5a9db8c9a32b902f4af232259b122bdde
Jayendra <jayendra.sharan@gmail.com>
2018-12-03 02:30:43 +0530

    updated port

--- client/src/constants/api.js
+++ client/src/constants/api.js
@@ -1,7 +1,7 @@
 const API = {
   'FETCH_GAME_COUNT': '/api/available-game-count',
   'REGISTER_PLAYER': '/api/register-player',
-  'SOCKET_API': 'http://localhost:4001/'
+  'SOCKET_API': 'http://localhost:5000/'
 }
 
 export default API;
