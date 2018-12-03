d5f88a490d769ff21e005912519d2ab59e21962b
Jayendra <jayendra.sharan@gmail.com>
2018-12-03 12:02:24 +0530

    packages updated and server static serve updated

--- server/index.js
+++ server/index.js
@@ -9,11 +9,20 @@ const port = process.env.PORT || 5000;
 const gameRouter = require ('./tempDb/temp-db');
 const socketServer = require ('./tempDb/socket');
 
+const path = require ('path');
+
 const app = express ();
 app.use (cors ());
 app.use (bodyParser.urlencoded({ extended: true }));
 app.use (bodyParser.json ());
 app.use (gameRouter);
+
+app.use(express.static(path.join(__dirname, '../client/build')))
+// Anything that doesn't match the above, send back index.html
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '../client/build/index.html'))
+});
 
 const server = http.createServer (app);
 const io = socketIo (server);
